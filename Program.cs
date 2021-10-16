56cf32c25a8f80175cdec61465b7bbde977fd214
vishal.Alok <you@example.com>
2021-10-16 06:49:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticClass
+{
+    static class Student
+    {
+        static int RollNumber;
+        static string StudentName = string.Empty;
+        public static void Details()
+        {
+            Console.WriteLine("Student RollNumber is " + RollNumber + " and Name is " + StudentName);
+        }
+
+        static Student()
+        {
+            RollNumber = 100;
+            StudentName = "Vishal";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Details();
+        }
+    }
+}
